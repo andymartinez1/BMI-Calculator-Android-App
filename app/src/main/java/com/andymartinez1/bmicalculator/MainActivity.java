ceb2bad186dd03da6e9061a2804f440f7e7e6c2c
Andrew Martinez <martinez.s.andrew@gmail.com>
2025-03-26 20:41:01 -0400

    Added views to the MainActivity.java file to be able to manipulate them. Added a toast notification to the calculate button for testing.

--- app/src/main/java/com/andymartinez1/bmicalculator/MainActivity.java
+++ app/src/main/java/com/andymartinez1/bmicalculator/MainActivity.java
@@ -1,7 +1,12 @@
 package com.andymartinez1.bmicalculator;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +16,6 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +26,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        RadioButton maleRadioButton = findViewById(R.id.radio_button_male);
+        RadioButton femaleRadioButton = findViewById(R.id.radio_button_female);
+
+        EditText ageText = findViewById(R.id.edit_text_age);
+        EditText heightInFeetText = findViewById(R.id.edit_text_feet);
+        EditText heightInInchesText = findViewById(R.id.edit_text_inches);
+        EditText weightText = findViewById(R.id.edit_text_weight);
+
+        Button calculateButton = findViewById(R.id.button_calculate);
+
+        TextView resultText = findViewById(R.id.text_view_result);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Calculating BMI", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
 }
